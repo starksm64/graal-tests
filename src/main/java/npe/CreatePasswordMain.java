11041cd5c96d1dcc0487ecee3d8b4cb9d6138cde
Scott Stark <starksm64@gmail.com>
2018-12-22 10:12:14 -0800

    Give the main class a better name

--- src/main/java/npe/CreatePasswordMain.java
+++ src/main/java/npe/CreatePasswordMain.java
@@ -2,7 +2,10 @@ package npe;
 
 import npe.security.ClearPassword;
 
-public class RandomPassword {
+/**
+ * A simple main that calls ClearPassword.createRaw() and prints the result
+ */
+public class CreatePasswordMain {
     public static void main(String[] args) {
         ClearPassword pass = ClearPassword.createRaw("CLEAR", "random".toCharArray());
         System.out.printf("pass=%s, class=%s\n", pass, pass.getClass());
